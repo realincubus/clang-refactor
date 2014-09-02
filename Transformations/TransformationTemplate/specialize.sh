70706e98e96524540374ae50978c41f52a80c267
incubus <lrs.incubus@googlemail.com>
2014-09-02 10:43:12 +0200

    removed everything useless

--- Transformations/TransformationTemplate/specialize.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ../$1 
-sed s/TransformationTemplate/$1/g TransformationTemplate.cpp		> ../$1/$1.cpp
-sed s/TransformationTemplate/$1/g TransformationTemplate.h		> ../$1/$1.h
-sed s/TransformationTemplate/$1/g TransformationTemplateActions.cpp	> ../$1/$1Actions.cpp
-sed s/TransformationTemplate/$1/g TransformationTemplateActions.h	> ../$1/$1Actions.h
-sed s/TransformationTemplate/$1/g TransformationTemplateMatchers.cpp	> ../$1/$1Matchers.cpp
-sed s/TransformationTemplate/$1/g TransformationTemplateMatchers.h	> ../$1/$1Matchers.h
